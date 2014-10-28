06b2317ec4a0df3e0037cb0f521612a5e1dbcf58
278253685 <278253685@qq.com>
2014-10-28 08:11:30 +0800

    登陆注册模块

--- src/com/funtouch/Login.java
+++ src/com/funtouch/Login.java
@@ -1,31 +1,78 @@
 package com.funtouch;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Vector;
+
 import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import com.funtouch.UserInfo;
+import com.funtouch.Data;
 
 public class Login extends Activity {
+	private SharedPreferences read ;
+	private EditText userName, password;
+	public Data application;
+	List<UserInfo> userLogin= new ArrayList<UserInfo>();
+	
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		Button btnLogin = null;
 		Button btnRegist = null;
+		read = getSharedPreferences("user",Context.MODE_PRIVATE);
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+		userName = (EditText) findViewById(R.id.edit_UserName);
+		password = (EditText) findViewById(R.id.edit_UserPassword);
 		btnLogin = (Button) findViewById(R.id.btn_login);
+		application = (Data) this.getApplicationContext(); 
+		userLogin = getSPUser();
+		//showToast(read.getString("member", ""));
 		btnLogin.setOnClickListener(new OnClickListener() {
 			public void onClick(View v) {
-				Intent intent = new Intent();
-				intent.setClass(Login.this, UserMenu.class);
-				startActivity(intent);
+				if(userName.getText().toString().trim().equals("")||password.getText().toString().trim().equals("")){
+					showToast("请输入账户和密码..");
+				}
+				else{
+					if( userLogin!=null || userLogin.size()>0 )	
+					{
+						int flag = 0;
+						for(int i=0;i<userLogin.size();i++)	
+						{
+							if(userName.getText().toString().equals(userLogin.get(i).getName())&& password.getText().toString().equals(userLogin.get(i).getPassword()))
+							{
+								showToast("登录成功");
+								Intent intent = new Intent();
+								intent.setClass(Login.this, UserMenu.class);
+								startActivity(intent);
+								flag = 1;
+							}
+						
+						}
+						if(flag == 0)
+							showToast("用户名或密码错误,请重新输入");
+					}
+					else
+						showToast("用户名或密码错误,请重新输入");
+					
+					
+				}
+				
 
 			}
 		});
@@ -39,7 +86,47 @@ public class Login extends Activity {
 			}
 		});
 	}
+	
+	//提示类
+	private void showToast(CharSequence msg) {
+		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+	}
+	
+	public List<UserInfo> getSPUser()
+	{
+		List<UserInfo> userInfos = new ArrayList<UserInfo>();// 用于保存用户列表信息
+		String userinfos = read.getString("member", "");// 取得所有用户信息
+		// 获得用户字串
+		if (userinfos != "")// 有数据
+		{
+			// name1/pwd1,name2/pwd2
+			if (userinfos.contains(","))// 判断有无, 逗号代表用户每个用户分割点
+			{
+				String[] users = userinfos.split(",");
+				for (String str : users)
+				{
+					UserInfo userinfo = new UserInfo();
+					String[] user = str.split("/");
+					userinfo.initialize(user[0],user[1],user[2],user[3],user[4],user[5],user[6]);// 
+					userInfos.add(userinfo);
+				}
+			} else
+				// 没有, 代表只有一个用户
+			{
+				UserInfo userinfo = new UserInfo();
+				String[] user = userinfos.split("/");
+				userinfo.initialize(user[0],user[1],user[2],user[3],user[4],user[5],user[6]);// 
+				userInfos.add(userinfo);
+			}
+			return userInfos;
+	  } else
+	  {
+		  return userInfos;
+	  }
+	}
 }
+
+
      
         
 
--- src/com/funtouch/RegistInfo.java
+++ src/com/funtouch/RegistInfo.java
@@ -1,18 +1,26 @@
 package com.funtouch;
 
 import android.app.Activity;
+import android.app.Application;
 import android.content.Intent;
 import android.os.Bundle;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import com.funtouch.UserInfo;
+import java.util.*;
+import com.funtouch.Data;
+
 
 public class RegistInfo extends Activity{
-
+	private EditText userName, password,passwordAgain, userMailbox, userClass, userPhone;
+	public Data application;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +28,54 @@ public class RegistInfo extends Activity{
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.regist_info);
+		userName = (EditText) findViewById(R.id.edit_UserName1);
+		password = (EditText) findViewById(R.id.edit_Password);
+		passwordAgain = (EditText) findViewById(R.id.edit_PasswordAgain);
+		userMailbox = (EditText) findViewById(R.id.edit_Mailbox);
+		userClass = (EditText) findViewById(R.id.edit_Class);
+		userPhone = (EditText) findViewById(R.id.edit_Phone);
 		btnNext=(Button)findViewById(R.id.btn_next);
+		application = (Data) this.getApplicationContext(); 
 		btnNext.setOnClickListener(new OnClickListener(){
 	        	public void onClick(View v){
-	        		Intent intent=new Intent();
-	        		intent.setClass(RegistInfo.this, RegistOrgan.class);
-	        		startActivity(intent);
+	        		if(userName.getText().toString().trim().equals("") || password.getText().toString().trim().equals("")
+	        				||passwordAgain.getText().toString().trim().equals("")||userMailbox.getText().toString().trim().equals("")
+	        				||userClass.getText().toString().trim().equals("")||userPhone.getText().toString().trim().equals("")){
+						showToast("请输入全部信息..");
+					}
+	        		else if(!password.getText().toString().equals(passwordAgain.getText().toString())){
+	        			showToast("两次输入的密码不一致,请重新输入");
+	        		}
+	        		else if(password.getText().length()<6){
+	        			showToast("密码不符合规则,请重新输入");
+	        		}
+	        		else if(!userMailbox.getText().toString().contains("@")){
+	        			showToast("邮箱格式不符合规则,请重新输入");
+	        		}
+	        		else if(!userClass.getText().toString().contains("-")){
+	        			showToast("班级格式不符合规则,请重新输入");
+	        		}
+	        		else if(userPhone.getText().length()<11){
+	        			showToast("电话号码不符合规则,请重新输入");
+	        		}
+	        		else{
+	        			application.setUserName(userName.getText().toString());
+	        			application.setPassword(password.getText().toString());
+	        			application.setUserMailbox(userMailbox.getText().toString());
+	        			application.setUserClass(userClass.getText().toString());
+	        			application.setUserPhone(userPhone.getText().toString());
+	        			Intent intent=new Intent();
+	        			intent.setClass(RegistInfo.this, RegistOrgan.class);
+	        			startActivity(intent);
+	        			}
+	        		
 	
 	}
 	        });
+		
 	}
+	private void showToast(CharSequence msg) {
+		Toast.makeText(this, msg, Toast.LENGTH_SHORT).show();
+	}
+	
 }
